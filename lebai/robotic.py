dcef88ddd08673b13776db10ed3fa61a1768ac70
kingfree <kingfree@toyama.moe>
2021-02-09 10:33:11 +0800

    aio实现

--- lebai/robotic.py
+++ lebai/robotic.py
@@ -6,6 +6,7 @@ from enum import Enum, unique
 from .pb2 import robot_controller_pb2_grpc
 from .pb2 import robot_controller_pb2 as rc
 from .pb2 import messages_pb2 as msg
+from google.protobuf.empty_pb2 import Empty
 
 class RobotState(Enum):
     '''机器人状态
@@ -29,9 +30,15 @@ class CartesianPose:
     '''笛卡尔坐标描述的位姿
     '''
     def __init__(self, x, y, z, rz, ry, rx, base=None):
-        self.pos = [x, y, z, rz, ry, rx]
+        self.pos = (x, y, z, rz, ry, rx)
         self.is_joint = False
-        self.base = base
+        self.base = getattr(base, 'pos', base) if base is not None else None
+
+    def __str__(self):
+        if self.base is None:
+            return str(self.pos)
+        else:
+            return f'({self.pos[0]}, {self.pos[1]}, {self.pos[2]}, {self.pos[3]}, {self.pos[4]}, {self.pos[5]}, base={self.base})'
 
 class JointPose:
     '''关节旋转角度描述的机器人姿态
@@ -39,6 +46,9 @@ class JointPose:
     def __init__(self, *j):
         self.pos = j
         self.is_joint = True
+
+    def __str__(self):
+        return str(self.pos)
 
 class LebaiRobot:
     '''
@@ -49,10 +59,10 @@ class LebaiRobot:
     可以为同一台设备创建多个实例，但同一台机器人同时只能执行有限的指令。
     '''
     def __init__(self, ip):
-        self.rcc = grpc.insecure_channel(f'{ip}:5181')
+        self.rcc = grpc.aio.insecure_channel(f'{ip}:5181')
         self.rcs = robot_controller_pb2_grpc.RobotControllerStub(self.rcc)
 
-    def movej(self, pos, a, v, t, r):
+    async def movej(self, pos, a, v, t, r):
         '''线性移动（关节空间）
 
         :param pos:
@@ -63,25 +73,46 @@ class LebaiRobot:
         :param t: 运动时间 (s)
         :param r: 交融半径 (m)
         '''
-        # TODO: await Sync
         req = rc.MoveJRequest(
+            joint_pose_to = list(pos.pos),
             pose_is_joint_angle=getattr(pos, 'is_joint', False),
             acceleration=a,
             velocity=v,
             time=t,
             blend_radius=r
         )
-        req.joint_pose_to[:] = pos.pos
         if getattr(pos, 'base', None):
-            req.pose_base = msg.PR(position=pos.base[0:3],rotation=pos.base[3:6])
-        self.rcs.MoveJ(req)
+            req.pose_base.position.x = pos.base[0]
+            req.pose_base.position.y = pos.base[1]
+            req.pose_base.position.z = pos.base[2]
+            req.pose_base.rotation.r = pos.base[3]
+            req.pose_base.rotation.p = pos.base[4]
+            req.pose_base.rotation.y = pos.base[5]
+        res = await self.rcs.MoveJ(req)
+        return res
 
-    def get_joint_temp(self, joint):
+    async def get_actual_joint_positions(self):
+        '''获取实际关节位置
+
+        :returns: 
+        '''
+        res = await self.rcs.GetActualJointPositions(Empty())
+        return JointPose(*res.joints)
+
+    async def get_actual_tcp_pose(self):
+        '''获取实际空间位置
+
+        :returns: 
+        '''
+        res = await self.rcs.GetActualTcpPose(Empty())
+        return CartesianPose(*res.vector)
+
+    async def get_joint_temp(self, joint):
         '''获取关节温度
 
         :param joint: 关节序号
 
         :returns: 关节当前温度 (℃)
         '''
-        response = self.rcs.GetJointTemp(rc.IntRequest(index=joint))
-        return response.degree
+        res = await self.rcs.GetJointTemp(rc.IntRequest(index=joint))
+        return res.degree
--- main.py
+++ main.py
@@ -2,16 +2,31 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import asyncio
+import math
 
 from lebai import LebaiRobot, CartesianPose, JointPose
+from grpc.aio import AioRpcError
 
-def run():
+async def run():
     rb = LebaiRobot("192.168.3.218")
-    print(f"get_joint_temp(1) = {rb.get_joint_temp(1):.3f}")
+    print(f"get_joint_temp(1) = {(await rb.get_joint_temp(1)):.3f}")
 
-    rb.movej(JointPose(0, 0, 0, 0, 0, 0), 0, 0, 1, 0)
-    rb.movej(CartesianPose(-0.54, -0.2, 0.117, 0, 0, 0), 0, 0, 1, 0)
+    await rb.movej(JointPose(0, 0, 0, 0, 0, 0), 0, 0, 1, 0)
+    print(await rb.get_actual_joint_positions())
+    try:
+        p = CartesianPose(-0.54, -0.2, 0.117, 0, math.pi / 2, math.pi / 2)
+        await rb.movej(p, 0, 0, 1, 0)
+    except AioRpcError as e:
+        print(e.code(), e.details())
+    print(await rb.get_actual_joint_positions())
+    base = await rb.get_actual_tcp_pose()
+    print('base=', base)
+    p1 = CartesianPose(0.1, 0, 0, 0, 0, 0, base=base)
+    print(p1)
+    await rb.movej(p1, 0, 0, 1, 0)
+    print(await rb.get_actual_tcp_pose())
 
 if __name__ == '__main__':
     logging.basicConfig()
-    run()
+    asyncio.run(run())
