26dca5c15de453b4f571248e4c09d74fefbbcf0b
Ycy <ycy22333@163.com>
2021-05-24 11:29:18 +0800

    commit

--- lebai/type.py
+++ lebai/type.py
@@ -3,40 +3,75 @@ from enum import Enum
 from lebai.pb2 import messages_pb2 as msg
 from .pb2 import robot_controller_pb2 as rc
 
-
 class RobotState(Enum):
-    '''机器人状态
-    '''
-    DISCONNECTED = 0  # 已断开连接
-    ESTOP = 1  # 急停停止状态
-    BOOTING = 2  # 启动中
-    ROBOT_OFF = 3  # 电源关闭
-    ROBOT_ON = 4  # 电源开启
-    IDLE = 5  # 空闲中
-    PAUSED = 6  # 暂停中
-    RUNNING = 7  # 机器人运动运行中
-    UPDATING = 8  # 更新固件中
-    STARTING = 9  # 启动中
-    STOPPING = 10  # 停止中
-    TEACHING = 11  # 示教中
-    STOP = 12  # 普通停止
-    FINETUNING = 13  # 微调中
+    """机器人状态"""
+
+    DISCONNECTED = 0
+    """已断开连接"""
+
+    ESTOP = 1
+    """急停停止状态"""
+
+    BOOTING = 2
+    """启动中"""
+
+    ROBOT_OFF = 3
+    """电源关闭"""
+    ROBOT_ON = 4
+    """电源开启"""
+    IDLE = 5
+    """空闲中"""
+    PAUSED = 6
+    """暂停中"""
+    RUNNING = 7
+    """机器人运动运行中"""
+    UPDATING = 8
+    """更新固件中"""
+    STARTING = 9
+    """启动中"""
+    STOPPING = 10
+    """停止中"""
+    TEACHING = 11
+    """示教中"""
+    STOP = 12
+    """普通停止"""
+    FINETUNING = 13
+    """微调中"""
 
 
 class TaskStatus(Enum):
-    IDLE = 0  # 空闲
-    RUNNING = 1  # 运行中
-    PAUSED = 2  # 暂停
-    SUCCESS = 3  # 运行完成
-    STOPPED = 4  # 停止
-    ABORTED = 5  # 异常终止
+    """
+    任务状态
+
+    """
+
+    IDLE = 0
+    """空闲"""
+    RUNNING = 1
+    """运行中"""
+    PAUSED = 2
+    """暂停"""
+    SUCCESS = 3
+    """运行完成"""
+    STOPPED = 4
+    """停止"""
+    ABORTED = 5
+    """异常终止"""
 
 
 class IODeviceType(Enum):
-    HOST = 0  # 主机箱
-    FLANGE = 1  # 法兰
-    GRIPPER = 2  # 手爪
-    MODBUS_TCP = 3  # MODBUS/TCP
+    """
+    IO设备类型
+
+    """
+    HOST = 0
+    """主机箱"""
+    FLANGE = 1
+    """法兰"""
+    GRIPPER = 2
+    """手爪"""
+    MODBUS_TCP = 3
+    """MODBUS/TCP"""
     # MODBUS_TCP6 = 4
     # MODBUS_RTU = 5
 
@@ -134,18 +169,34 @@ class RequestError(Error):
 
 
 class TaskInfo:
-    id: int  # 任务ID
-    scene_id: int  # 场景ID
-    execute_count: int  # 执行次数
-    executed_count: int  # 已执行次数
-    name: int  # 任务名
-    status: TaskStatus  # 任务状态。1:运行中 3:运行成功 4:用户主动停止 5:异常停止
-    comment: str  # 备注
-    start_time: str  # 任务开始时间
-    end_time: str  # 任务结束时间
-    consume_time: int  # 单位秒
-    create_time: str  # 创建时间
-    update_time: str  # 更新时间
+    """
+    任务信息
+
+    """
+    id: int
+    """任务ID"""
+    scene_id: int
+    """场景ID"""
+    execute_count: int
+    """执行次数"""
+    executed_count: int
+    """已执行次数"""
+    name: int
+    """任务名"""
+    status: TaskStatus
+    """任务状态。1:运行中 3:运行成功 4:用户主动停止 5:异常停止"""
+    comment: str
+    """备注"""
+    start_time: str
+    """任务开始时间"""
+    end_time: str
+    """任务结束时间"""
+    consume_time: int
+    """单位秒"""
+    create_time: str
+    """创建时间"""
+    update_time: str
+    """更新时间"""
 
     def __init__(self, res):
         self.id = getattr(res, "id")
@@ -163,10 +214,19 @@ class TaskInfo:
 
 
 class TasksResult:
-    pi: int  # 页索引
-    ps: int  # 页大小
-    total: int  # 总数
-    records: list[TaskInfo]  # 任务列表
+    """
+    任务列表信息
+
+    """
+
+    pi: int
+    """页索引"""
+    ps: int
+    """页大小"""
+    total: int
+    """总数"""
+    records: list[TaskInfo]
+    """任务列表"""
 
     def __init__(self, res):
         self.pi = getattr(res, "pi")
@@ -174,3 +234,111 @@ class TasksResult:
         self.total = getattr(res, "total")
         self.records = getattr(res, "records")
         pass
+
+
+class PVAT:
+    """
+    移动请求参数
+    """
+
+    duration: float
+    """总运动时间"""
+    q: list[float]
+    """关节位置列表"""
+    v: list[float]
+    """关节速度列表"""
+    acc: list[float]
+    """关节加速度列表"""
+
+    def __init__(self, duration: float, q: list[float], v: list[float], acc: list[float]):
+        self.duration = duration
+        self.q = q
+        self.v = v
+        self.acc = acc
+        pass
+
+
+class RobotPoseData:
+    """
+    机器人姿态数据
+
+    """
+
+    target_joint: list[float]
+    """目标关节位置"""
+    actual_joint: list[float]
+    """实际关节位置"""
+    target_pose: list[float]
+    """目标tcp位置"""
+    actual_pose: list[float]
+    """实际tcp位置"""
+
+    def __init__(self, res):
+        self.target_joint = tuple(res.targetJoint.joints)
+        self.actual_joint = tuple(res.actualJoint.joints)
+        self.target_pose = tuple(res.targetTcpPose.vector)
+        self.actual_pose = tuple(res.actualTcpPose.vector)
+
+
+class RobotData(RobotPoseData):
+    """
+    机器人数据
+
+    """
+
+    target_torque: list[float]
+    """理论力矩"""
+    actual_torque: list[float]
+    """实际力矩"""
+    target_vel: list[float]
+    """目标关节速度"""
+    actual_vel: list[float]
+    """实际关节速度"""
+    target_acc: list[float]
+    """加速度"""
+    actual_acc: list[float]
+    """实际加速度"""
+    temp: list[float]
+    """关节温度"""
+
+    def __init__(self, res):
+        super().__init__(res)
+        self.target_torque = tuple(res.targetTorque.joints)
+        self.actual_torque = tuple(res.actualTorque.joints)
+        self.target_vel = tuple(res.targetJointSpeed.joints)
+        self.actual_vel = tuple(res.actualJointSpeed.joints)
+        self.target_acc = tuple(res.targetJointAcc.joints)
+        self.actual_acc = tuple(res.actualJointAcc.joints)
+        self.temp = tuple(res.jointTemps.joints)
+
+
+class IOItem:
+    pin: int
+    value: float
+
+    def __init__(self, pin: int, value: float):
+        self.pin = pin
+        self.value = value
+
+
+class RobotIOData:
+    di: list[IOItem]
+    """所有数字量的输入值"""
+    do: list[IOItem]
+    """所有数字量的输出值"""
+    ai: list[IOItem]
+    """所有模拟量输入值"""
+    ao: list[IOItem]
+    """所有模拟量输出值"""
+    flange_di: list[IOItem]
+    """所有tcp数字量的输入值"""
+    flange_do: list[IOItem]
+    """所有tcp数字量的输出值"""
+
+    def __init__(self, io):
+        self.di = tuple(map(lambda dio: IOItem(dio.pin, dio.value), io.robotDIOIn))
+        self.do = tuple(map(lambda dio: IOItem(dio.pin, dio.value), io.robotDIOOut))
+        self.ai = tuple(map(lambda aio: IOItem(aio.pin, aio.value), io.robotAIOIn))
+        self.ao = tuple(map(lambda aio: IOItem(aio.pin, aio.value), io.robotAIOOut))
+        self.flange_di = tuple(map(lambda dio: IOItem(dio.pin, dio.value), io.tcpDIOIn))
+        self.flange_do = tuple(map(lambda dio: IOItem(dio.pin, dio.value), io.tcpDIOOut))
