7cd13c3fef5cdea62b16daaaad4cce0c0abf90e1
jinfeng.tian <jinfeng.tian@lebai.ltd>
2021-05-25 18:20:37 +0800

    0.6.2

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, kingfree'
 author = 'kingfree'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.1'
+release = '0.6.2'
 
 # -- General configuration ---------------------------------------------------
 
